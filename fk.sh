056d965c6af580d3e925c67804ff34076b307f8f
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-06-06 10:26:28 +1200

    ok so S0 it is

--- fk.sh
+++ fk.sh
@@ -3,3 +3,4 @@ apt-get install u-boot-tools
 flash-kernel
 cd /boot
 mkimage -A arm -T script -C none -n "Ubuntu boot script" -d boot.script boot.scr
+sed -i -e 's/ttyAMA0/ttyS0/g' /etc/init/ttyAMA0.conf > /etc/init/ttyS0.conf
