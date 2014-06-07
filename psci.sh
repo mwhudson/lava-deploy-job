36ab36a6d046e9ea571f0bc98c71ad129e2c448b
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-06-07 13:36:19 +1200

    test with psci bits

---
+++ psci.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir scratch
+cd scratch
+wget http://lava-leg02/~mwhudson/linux-headers-3.15.0-5_3.15.0-5.10+mwhudson_all.deb
+wget http://lava-leg02/~mwhudson/linux-headers-3.15.0-5-generic_3.15.0-5.10+mwhudson_arm64.deb
+wget http://lava-leg02/~mwhudson/linux-image-3.15.0-5-generic_3.15.0-5.10+mwhudson_arm64.deb
+dpkg -i *.deb
+apt-get install -f
+apt-get install qemu-system
+wget http://lava-leg02/~mwhudson/qemu-system-aarch64
+chmod a+x qemu-system-aarch64
+cp qemu-system-aarch64 `which qemu-system-aarch64`
+cd ../
+rm -rf scratch
