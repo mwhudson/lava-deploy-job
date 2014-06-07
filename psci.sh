abb27c8ecba8594b13224b92cc23bfbf319a8a0f
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-06-07 14:18:36 +1200

    just guessing now

--- psci.sh
+++ psci.sh
@@ -22,3 +22,4 @@ rm -rf scratch
 cd /boot
 mkimage -A arm -T script -C none -n "Ubuntu boot script" -d boot.script boot.scr
 sed -i -e 's/ttyAMA0/ttyS0/g' /etc/init/ttyAMA0.conf > /etc/init/ttyS0.conf
+sync
