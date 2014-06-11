1aa03e26a4a7f95018ac49c8e27b93b372b10241
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-06-11 13:01:57 +1200

    lets see if dann is better at kernel than me

--- psci.sh
+++ psci.sh
@@ -13,9 +13,9 @@ apt-get update
 
 mkdir scratch
 cd scratch
-wget http://lava-leg02/~mwhudson/linux-headers-3.15.0-5_3.15.0-5.10+mwhudson_all.deb
-wget http://lava-leg02/~mwhudson/linux-headers-3.15.0-5-generic_3.15.0-5.10+mwhudson_arm64.deb
-wget http://lava-leg02/~mwhudson/linux-image-3.15.0-5-generic_3.15.0-5.10+mwhudson_arm64.deb
+wget http://lava-leg02/~mwhudson/linux-headers-3.15.0-5_3.15.0-5.10+hyperscale.1_all.deb
+wget http://lava-leg02/~mwhudson/linux-headers-3.15.0-5-generic_3.15.0-5.10+hyperscale.1_arm64.deb
+wget http://lava-leg02/~mwhudson/linux-image-3.15.0-5-generic_3.15.0-5.10+hyperscale.1_arm64.deb
 dpkg -i *.deb
 apt-get install -f
 apt-get install -y qemu-system
