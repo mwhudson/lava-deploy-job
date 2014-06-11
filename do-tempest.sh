f891f84ff7aeb242103647a4e11bffb0e42d7eae
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-06-12 09:42:49 +1200

    generize attachments, attach more stuff

--- do-tempest.sh
+++ do-tempest.sh
@@ -10,13 +10,20 @@ echo never > /sys/kernel/mm/transparent_hugepage/enabled
 apt-get update
 apt-get -y install lsb-release git nmap vim pm-utils bridge-utils openssh-server
 
+t_dir=$(pwd)
+
 git clone -b arm64-trusty git://github.com/mwhudson/testing-openstack
 cd testing-openstack
 ./setup.sh
 cd /opt/stack/tempest
 cp etc/logging.conf.sample etc/logging.conf
+mkdir /home/ubuntu/attachments
 if [ "$LAVA_RUN_TEMPEST" = "yes" ]; then
-    sudo -u stack ./run_tempest.sh -l -N -t -- $LAVA_TESTS_TO_RUN 2>&1 | tee /home/ubuntu/tempest-logs.txt
+    sudo -u stack ./run_tempest.sh -l -N -t -- $LAVA_TESTS_TO_RUN 2>&1 | tee /home/ubuntu/attachments/tempest-logs.txt
+    testr last --subunit | subunit-1to2 | subunit2csv > /home/ubuntu/attachments/results.csv
+    python ${t_dir}/simplify-results.py /home/ubuntu/attachments/results.csv > /home/ubuntu/attachments/results.txt
+    cd /opt/stack/log
+    tar -czf /home/ubuntu/attachments/devstack-logs.tgz .
 fi
 if [ "$LAVA_SLEEP_FOR_ACCESS" = "yes" ]; then
     sleep 3600
