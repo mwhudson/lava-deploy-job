3349ea78d78ba6663bebdf8bbdc7cdd9ea41183e
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-06-12 15:22:12 +1200

    kernel versioning a bit more complicated than i thought

--- scripts/upgrade-kernel.sh
+++ scripts/upgrade-kernel.sh
@@ -9,13 +9,16 @@ if [ -z "$LAVA_CUSTOM_KERNEL_VERSION" ]; then
     exit 0
 fi
 
+V=$(echo $LAVA_CUSTOM_KERNEL_VERSION | cut -d. -f -3)
+W=$LAVA_CUSTOM_KERNEL_VERSION
+
 apt-get update
 
 mkdir scratch
 cd scratch
-wget http://lava-leg02/~mwhudson/linux-headers-${LAVA_CUSTOM_KERNEL_VERSION}_all.deb
-wget http://lava-leg02/~mwhudson/linux-headers-${LAVA_CUSTOM_KERNEL_VERSION}_arm64.deb
-wget http://lava-leg02/~mwhudson/linux-image-${LAVA_CUSTOM_KERNEL_VERSION}_arm64.deb
+wget http://lava-leg02/~mwhudson/linux-headers-${V}_${W}_all.deb
+wget http://lava-leg02/~mwhudson/linux-headers-${V}-generic_${W}_arm64.deb
+wget http://lava-leg02/~mwhudson/linux-image-${V}-generic_${W}_arm64.deb
 dpkg -i *.deb
 apt-get install -f
 apt-get install -y qemu-system
