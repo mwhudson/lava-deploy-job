5c53a195a5ab5e23e01bbcaa6ce45b7b1bddbd67
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-06-13 14:17:14 +1200

    fiddle faddle

--- scripts/do-tempest.sh
+++ scripts/do-tempest.sh
@@ -8,7 +8,7 @@ resolvconf -u
 echo never > /sys/kernel/mm/transparent_hugepage/enabled
 
 apt-get update
-apt-get -y install lsb-release git nmap vim pm-utils bridge-utils openssh-server
+apt-get -y install lsb-release git nmap vim pm-utils bridge-utils openssh-server subunit
 
 git clone -b arm64-trusty git://github.com/mwhudson/testing-openstack
 cd testing-openstack
@@ -17,10 +17,9 @@ cd /opt/stack/tempest
 cp etc/logging.conf.sample etc/logging.conf
 mkdir /home/ubuntu/attachments
 if [ "$LAVA_RUN_TEMPEST" = "yes" ]; then
-    testr list-tests $LAVA_TESTS_TO_RUN > /home/ubuntu/attachments/all-tests.txt
-    sudo -u stack ./run_tempest.sh -l -N -t -- $LAVA_TESTS_TO_RUN 2>&1 | tee /home/ubuntu/attachments/tempest-logs.txt
-    apt-get install subunit
-    testr last --subunit | subunit-1to2 | subunit2csv > /home/ubuntu/attachments/results.csv
+    sudo -u stack testr list-tests $LAVA_TESTS_TO_RUN > /home/ubuntu/attachments/all-tests.txt
+    sudo -u stack testr run --subunit $LAVA_TESTS_TO_RUN 2>&1 | tee results.subunit | subunit-2to1 | tools/colorizer.py
+    cat results.subunit | subunit2csv > /home/ubuntu/attachments/results.csv
     cd /opt/stack/log
     tar -czf /home/ubuntu/attachments/devstack-logs.tgz .
 fi
