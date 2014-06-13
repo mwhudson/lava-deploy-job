8c0fb473ea3f5faa965cdb047a54aa91a8245c23
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-06-13 13:29:26 +1200

    superstition++

--- scripts/do-tempest.sh
+++ scripts/do-tempest.sh
@@ -8,7 +8,7 @@ resolvconf -u
 echo never > /sys/kernel/mm/transparent_hugepage/enabled
 
 apt-get update
-apt-get -y install lsb-release git nmap vim pm-utils bridge-utils openssh-server subunit
+apt-get -y install lsb-release git nmap vim pm-utils bridge-utils openssh-server
 
 git clone -b arm64-trusty git://github.com/mwhudson/testing-openstack
 cd testing-openstack
@@ -19,6 +19,7 @@ mkdir /home/ubuntu/attachments
 if [ "$LAVA_RUN_TEMPEST" = "yes" ]; then
     testr list-tests $LAVA_TESTS_TO_RUN > /home/ubuntu/attachments/all-tests.txt
     sudo -u stack ./run_tempest.sh -l -N -t -- $LAVA_TESTS_TO_RUN 2>&1 | tee /home/ubuntu/attachments/tempest-logs.txt
+    apt-get install subunit
     testr last --subunit | subunit-1to2 | subunit2csv > /home/ubuntu/attachments/results.csv
     cd /opt/stack/log
     tar -czf /home/ubuntu/attachments/devstack-logs.tgz .
