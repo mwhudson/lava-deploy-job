d5e127a8ae29df9e82004aea2c5ca8114c0eb53d
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-06-23 16:08:15 +1200

    moar fun!

--- scripts/do-packaged-stack-controller.sh
+++ scripts/do-packaged-stack-controller.sh
@@ -3,6 +3,7 @@
 mkdir ~/.juju
 cp /opt/lava-scripts/environments.yaml ~/.juju
 juju bootstrap
-juju add-machine ssh:controller01
+#juju add-machine ssh:controller01
 juju add-machine ssh:compute01
 
+juju-deployer -e manual -c /opt/lava-scripts/minimal.yaml
--- scripts/do-packaged-stack.sh
+++ scripts/do-packaged-stack.sh
@@ -8,18 +8,18 @@ ln -s /lava /lava-$(lava-self)
 mkdir -p ~ubuntu/.ssh
 cp /opt/lava-scripts/id_rsa* ~ubuntu/.ssh
 cat ~ubuntu/.ssh/id_rsa.pub >> ~ubuntu/.ssh/authorized_keys
+cat >> ~ubuntu/.ssh/config <<EOF
+StrictHostKeyChecking no
+EOF
 chown -R ubuntu:ubuntu ~ubuntu/.ssh
 chmod 0600 ~ubuntu/.ssh/id_rsa
 chmod 0644 ~ubuntu/.ssh/id_rsa.pub
 chmod 0700 ~ubuntu/.ssh
-cat >> ~ubuntu/.ssh/config <<EOF
-StrictHostKeyChecking no
-EOF
 ssh ubuntu@compute01 true
 ssh ubuntu@controller01 true
 
 if [ `lava-role` = "controller" ]; then
-    apt-get install -y juju-core
+    apt-get install -y juju-core juju-deployer
     sudo -u ubuntu PATH=$PATH /opt/lava-scripts/do-packaged-stack-controller.sh
 fi
 
