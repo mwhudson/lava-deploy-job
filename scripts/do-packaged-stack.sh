50bb20a2945d8c17e6e42016687c50fc1e376421
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-06-23 15:39:02 +1200

    GAH

--- scripts/do-packaged-stack.sh
+++ scripts/do-packaged-stack.sh
@@ -9,7 +9,9 @@ chown ubuntu:ubuntu ~ubuntu/.ssh
 chmod 0600 ~ubuntu/.ssh/id_rsa
 chmod 0644 ~ubuntu/.ssh/id_rsa.pub
 chmod 0700 ~ubuntu/.ssh
-
+cat >> ~ubuntu/.ssh/config <<EOF
+StrictHostKeyChecking no
+EOF
 ssh ubuntu@compute01 true
 ssh ubuntu@controller01 true
 
