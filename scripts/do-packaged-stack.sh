fc110d941487db53fcacb5f45bcd3ace8b5d07a1
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-06-23 16:18:07 +1200

    ARGH

--- scripts/do-packaged-stack.sh
+++ scripts/do-packaged-stack.sh
@@ -15,8 +15,8 @@ chown -R ubuntu:ubuntu ~ubuntu/.ssh
 chmod 0600 ~ubuntu/.ssh/id_rsa
 chmod 0644 ~ubuntu/.ssh/id_rsa.pub
 chmod 0700 ~ubuntu/.ssh
-ssh ubuntu@compute01 true
-ssh ubuntu@controller01 true
+sudo -u ubuntu ssh ubuntu@compute01 true
+sudo -u ubuntu ssh ubuntu@controller01 true
 
 if [ `lava-role` = "controller" ]; then
     apt-get install -y juju-core juju-deployer
